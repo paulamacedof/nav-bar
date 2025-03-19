ec6273960ca21acb3669535b917676d0c0745826
Guilherme <guilherme.bail@gauge.haus>
2025-03-19 14:55:18 -0300

    hotfix username

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,16 @@ interface AppProps {
   setOpen: (isOpen: boolean) => void;
 }
 
-function App({ state, setOpen }: AppProps | any) {
-  console.log(state.pathname, "micro");
+function App({ toggleSidebar, pathname, setOpen }: AppProps | any) {
+  const user = localStorage.getItem("user");
+  const username = JSON.parse(user as string).username;
   return (
     <div className="bg-[#004D61] w-full p-8">
       <section className="flex justify-between max-w-7xl m-auto">
         <img src="/logo.png" alt="Bytebank" className="hidden md:block" />
         <span className="md:hidden">
           <Hamburger
-            toggled={state.toggleSidebar as boolean}
+            toggled={toggleSidebar as boolean}
             toggle={setOpen}
             color="#f97316"
             size={24}
@@ -22,7 +23,7 @@ function App({ state, setOpen }: AppProps | any) {
         </span>
 
         <div className="flex items-center gap-4 md:gap-10 text-white">
-          <span>{state?.user && state?.user}</span>
+          <span>{username && username}</span>
           <img src="/avatar.svg" alt="Avatar do Usuário" />
         </div>
       </section>
